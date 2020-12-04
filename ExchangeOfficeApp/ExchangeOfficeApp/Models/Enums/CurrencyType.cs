7ea1a6ab0b731cec3dbd3f2f8630fa3e4a2a9668
hvalovyura <yuri.hvalov@it-symphony.com>
2020-12-04 17:10:13 +0300

    Adding currency type enum

---
+++ ExchangeOfficeApp/ExchangeOfficeApp/Models/Enums/CurrencyType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace ExchangeOfficeApp.Enums
+{
+    enum CurrencyType
+    {
+        [Description("Доллар США")] USD = 0,
+        [Description("Евро")] EUR,
+        [Description("Российский рубль")] RUB
+    }
+}
