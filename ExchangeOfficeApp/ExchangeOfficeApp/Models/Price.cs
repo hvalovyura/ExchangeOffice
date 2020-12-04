a41bca996be8b282e9962ee33f7d5023c118dd8a
hvalovyura <yuri.hvalov@it-symphony.com>
2020-12-04 18:13:26 +0300

    Adding price history model

---
+++ ExchangeOfficeApp/ExchangeOfficeApp/Models/Price.cs
@@ -0,0 +1,17 @@
+﻿using ExchangeOfficeApp.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExchangeOfficeApp.Models
+{
+    public class Price
+    {
+        public int Id { get; set; }
+        public CurrencyType CurrencyType { get; set; }
+        public DateTime DateTime { get; set; }
+        public double BuyPrice { get; set; }
+        public double SellPrice { get; set; }
+
+    }
+}
