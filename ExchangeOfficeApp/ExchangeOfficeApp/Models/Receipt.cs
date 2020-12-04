16ae1d050134c8a4294692d08cb6d7d82a9e5cc4
hvalovyura <yuri.hvalov@it-symphony.com>
2020-12-04 16:26:57 +0300

    Changing receipt model

--- ExchangeOfficeApp/ExchangeOfficeApp/Models/Receipt.cs
+++ ExchangeOfficeApp/ExchangeOfficeApp/Models/Receipt.cs
@@ -6,5 +6,10 @@ namespace ExchangeOfficeApp.Models
 {
     class Receipt
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double ClientMoney { get; set; }
+        public double OfficeMoney { get; set; }
+        public DateTime DateTime { get; set; }
     }
 }
