a118a843131e1ad6324c4399d14c0bbd5d41f1eb
hvalovyura <yuri.hvalov@it-symphony.com>
2020-12-07 20:15:14 +0300

    Adding operation type enum

---
+++ ExchangeOfficeApp/ExchangeOfficeApp/Models/Enums/OperationType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace ExchangeOfficeApp.Models.Enums
+{
+    public enum OperationType
+    {
+        [Description("Покупка")] BUY = 0,
+        [Description("Продажа")] SELL
+    }
+}
